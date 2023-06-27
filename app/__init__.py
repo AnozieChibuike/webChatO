f2dab339bc21e30d33bde07c16cd2bd98f1cde69
Anozie Joel Chibuike <chibuikeanozie0@gmail.com>
2023-06-27 13:23:51 +0100

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,17 @@
 from flask import Flask
+from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_login import LoginManager
+from flask_socketio import SocketIo
 
 app = Flask(__name__)
+app.config.from_object(Config)
+db = SQLAlchemy(app)
+migrate = Migrate(app,db)
+login = LoginManager(app)
+login.login_view = 'login'
+socketio = SocketIo(app, cors_allowed_origin="*")
 
-from app import routes
+from app import routes,model
 
