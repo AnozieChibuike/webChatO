c9393126fd9c913c8312fef72ea6bdab0a3ba989
AnozieChibuike <chibuikeanozie0@gmail.com>
2023-06-27 15:28:40 +0000

    Signup Route

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,12 @@
 from app import app
 from flask import render_template as rd
+
 @app.route('/')
 @app.route('/home')
 def home():
     return rd("index.html.jinja")
+
+@app.route('/signup')
+def signup():
+    return rd("signup.html.jinja")
 
