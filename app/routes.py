66d83b1785e617fbb7c8ff71350532f7b19b754a
Anozie Joel Chibuike <chibuikeanozie0@gmail.com>
2023-06-27 17:18:35 +0100

    Update routes.py

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,5 @@
 from app import app
+from flask import request, flash, session, redirect
 from flask import render_template as rd
 
 @app.route('/')
@@ -6,8 +7,21 @@ from flask import render_template as rd
 def home():
     return rd("index.html.jinja")
 
-@app.route('/signup')
+@app.route('/signup',methods=["POST","GET"]
 def signup():
+    if request.method == "POST":
+        username = request.form['username']
+        email = request.form['email']
+        password = request.form['password']
+        if User.query.filter_by(email=email) is None and User.query.filter_by(username=username) is None:
+            user = User(username=username,email=email)
+            user.set_password(password)
+            db.session.add(user)
+            db.session.commit()
+            return redirect('/login')
+        else:
+            flash('User exists!')
+            return redirect('/signup')
     return rd("signup.html.jinja")
 
 @app.route('/login')
