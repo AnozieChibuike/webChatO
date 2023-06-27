0387e0791f64f6af8d3fc54869af558479f75837
Anozie Joel Chibuike <chibuikeanozie0@gmail.com>
2023-06-27 17:23:20 +0100

    Update model.py

--- app/model.py
+++ app/model.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 from app import db
+from werkzeug.security import check_password_hash, generate_password_hash
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -7,7 +8,13 @@ class User(db.Model):
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
     msg = db.relationship('Msg', backref='author', lazy='dynamic')
-
+    
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+        
+    def check_password(self,password):
+        return check_password_hash(self.password_hash,password)
+        
     def __repr__(self):
         return '<User {}>'.format(self.username)
 
