956fb2a20a03e56e35fbccb46c9c176c253b5cd4
Anozie Joel Chibuike <chibuikeanozie0@gmail.com>
2023-07-01 15:46:38 +0100

    Update model.py

--- app/model.py
+++ app/model.py
@@ -1,8 +1,9 @@
 from datetime import datetime
-from app import db
+from app import db, login
+from flask_login import UserMixin
 from werkzeug.security import check_password_hash, generate_password_hash
 
-class User(db.Model):
+class User(UserMixin,db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
