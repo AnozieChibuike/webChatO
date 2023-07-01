d8026864fa7bcfce39c46d579dbe208ecb0a3a9c
Anozie Joel Chibuike <chibuikeanozie0@gmail.com>
2023-07-01 20:25:33 +0100

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -1,6 +1,8 @@
-from app import app, db
+from app import app, db, socket
 from app.model import User, Msg
 
 @app.shell_context_processor
 def make_shell_context():
     return {'db': db, 'User': User, 'Msg': Msg}
+
+socket.run(app)
