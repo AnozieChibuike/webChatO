a28f10a5cedd2945ec23b06cac230d4f25b9ca56
Anozie Joel Chibuike <chibuikeanozie0@gmail.com>
2023-07-10 19:56:32 +0100

    Update routes.py

--- app/routes.py
+++ app/routes.py
@@ -6,13 +6,6 @@ from app.model import User, Msg
 from flask_login import current_user, login_user, logout_user, login_required
 from werkzeug.urls import url_parse
 
-
-@app.route('/logout')
-def logout():
-    leave()
-    logout_user()
-    return redirect('/')
-
 def get_admin_info():
     info = User.query.filter_by(username='info').first()
     if info:
@@ -24,33 +17,46 @@ def get_admin_info():
         info = User.query.filter_by(username='info').first()
         return info
 
+@app.route('/logout')
+def logout():
+    username = current_user.username
+    message = f'{username} left the chat.'
+    info = get_admin_info()
+    p = Msg(body=message, author=info)
+    db.session.add(p)
+    db.session.commit()
+    socket.emit('mes', {'user': 'info', 'msg': message})
+    logout_user()
+    return redirect('/')
+
+
 @app.route('/')
 @app.route('/home')
 def home():
     return rd("index.html.jinja")
 
 
-@socket.on('join')
-def join(data):
-    username = data['username']
-    message = f'{username} joined the chat.'
-    socket.emit('mes', {'user': 'info', 'msg': message})
-    info = get_admin_info()
-    p = Msg(body=message, author=info)
-    db.session.add(p)
-    db.session.commit()
+# @socket.on('join')
+# def join(data):
+#     username = data['username']
+#     message = f'{username} joined the chat.'
+#     socket.emit('mes', {'user': 'info', 'msg': message})
+#     info = get_admin_info()
+#     p = Msg(body=message, author=info)
+#     db.session.add(p)
+#     db.session.commit()
     
 
 
-@socket.on('disconnect')
-def leave():
-    info = get_admin_info()
-    username = current_user.username
-    message = f'{username} left the chat.'
-    p = Msg(body=message, author=info)
-    db.session.add(p)
-    db.session.commit()
-    socket.emit('mes', {'user': 'info', 'msg': message})
+# @socket.on('disconnect')
+# def leave():
+#     info = get_admin_info()
+#     username = current_user.username
+#     message = f'{username} left the chat.'
+#     p = Msg(body=message, author=info)
+#     db.session.add(p)
+#     db.session.commit()
+#     socket.emit('mes', {'user': 'info', 'msg': message})
     
 
 
@@ -87,7 +93,12 @@ def login():
             return redirect('/login')
         login_user(user, remember=remember)
         next_page = request.args.get('next')
-        session['welcome'] = f'{current_user.username} joined the chat'
+        message = f'{user.username} joined the chat.'
+        socket.emit('mes', {'user': 'info', 'msg': message})
+        info = get_admin_info()
+        p = Msg(body=message, author=info)
+        db.session.add(p)
+        db.session.commit()
         if not next_page or url_parse(next_page).netloc != '':
             next_page = url_for('chatbox')
         return redirect(next_page)
