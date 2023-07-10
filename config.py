08887f467216e3e8a18b01d0d0dde22eee9b9b09
Joel <chibuikeanozie0@gmail.com>
2023-07-10 14:15:27 +0100

    hj

--- config.py
+++ config.py
@@ -3,5 +3,5 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     SECRET_KEY = 'Testing Server'
-    SQLALCHEMY_DATABASE_URI = f"sqlite:///{os.path.join(basedir,'app.db')}"
+    SQLALCHEMY_DATABASE_URI = f'mysql://sql6631717:Cyam9hdema@sql6.freesqldatabase.com/sql6631717'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
