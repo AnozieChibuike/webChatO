01b3b616d78b5f8334ac3fed20925cbcfea1c381
Joel <chibuikeanozie0@gmail.com>
2023-07-13 02:13:06 +0100

    update

--- config.py
+++ config.py
@@ -1,8 +1,4 @@
 import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
 class Config(object):
     SECRET_KEY = os.getenv('SECRET')
     SQLALCHEMY_DATABASE_URI = f'mysql://{os.getenv("user")}:{os.getenv("password")}@{os.getenv("host")}:3306/{os.getenv("dbName")}'
