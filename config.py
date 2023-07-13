3b779b1ea98adac9b852c07e34257d4d358a8b88
Joel <chibuikeanozie0@gmail.com>
2023-07-13 02:28:49 +0100

    small error

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 import os
 class Config(object):
     SECRET_KEY = os.getenv('SECRET')
-    SQLALCHEMY_DATABASE_URI = f'mysql://{os.getenv("user")}:{os.getenv("password")}@{os.getenv("host")}:3306/{os.getenv("dbName")}'
+    SQLALCHEMY_DATABASE_URI = f'mysql://{os.getenv("username")}:{os.getenv("password")}@{os.getenv("host")}:3306/{os.getenv("dbName")}'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
